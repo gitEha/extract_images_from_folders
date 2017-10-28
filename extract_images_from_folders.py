9fec0538362d5ee73b8cd76a114810ea79632e0f
Ranet P <burn1nsunn@gmail.com>
2017-10-28 23:54:56 +0300

    Added #Example to comment

--- extract_images_from_folders.py
+++ extract_images_from_folders.py
@@ -2,7 +2,7 @@ import os
 from shutil import copyfile
 
 PATH_TO_IMAGES_TO_EXTRACT = '' #Example: 'E:\\Users\\User\\Desktop\\Machine Learning\\images\\randomimages\\101_ObjectCategories\\'
-PATH_TO_EXTRACT_TO = '' #'E:\\Users\\User\\Desktop\\Machine Learning\\images\\train_cat_or_not\\'
+PATH_TO_EXTRACT_TO = '' #Example: 'E:\\Users\\User\\Desktop\\Machine Learning\\images\\train_cat_or_not\\'
 
 for subdir, dirs, files in os.walk(PATH_TO_IMAGES_TO_EXTRACT):
     for num, file in enumerate(files):
